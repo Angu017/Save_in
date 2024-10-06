9b115820b10f243ca2259ccf9aa17c3e31cd9de2
Angu017 <101602981+Angu017@users.noreply.github.com>
2024-10-05 21:18:20 -0300

    Django bien hecho

--- urls.py
+++ urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import index,about,client,service,contact,team
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',index,name="index"),
-    path('about/',about,name="about"),
-    path('client/',client,name="client"),
-    path('contact/',contact,name="contact"),
-    path('service/',service,name="service"),
-    path('team/',team,name="team"),
-]
+    path('admin/', admin.site.urls),
+    path('',include('pagina.urls')),
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
