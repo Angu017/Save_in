1d50cf818df258a02c88ce83fd351ef6f7e93a10
Angu017 <101602981+Angu017@users.noreply.github.com>
2024-10-13 12:00:19 -0300

    Inicio de sesion, casi listo

--- pagina/views.py
+++ pagina/views.py
@@ -13,3 +13,10 @@ def client(request):
 
     return render(request, 'pagina/client.html')
 
+def log_in(request):
+
+    return render(request, 'pagina/log_in.html')
+
+def register(request):
+
+    return render(request, 'pagina/register.html')
